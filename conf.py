f677ba5c58af37d99c3315036e50911fcb02f5e9
Youngsu Kim <youngsu.kim@csusb.edu>
2022-12-15 20:13:48 +0900

    Test sphinx-book-theme

--- conf.py
+++ conf.py
@@ -14,9 +14,10 @@ release = 'v0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx_rtd_theme",
-]
+extensions = []
+# extensions = [
+#     "sphinx_rtd_theme",
+# ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,6 +27,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx-book-theme'
+
 html_static_path = ['_static']
